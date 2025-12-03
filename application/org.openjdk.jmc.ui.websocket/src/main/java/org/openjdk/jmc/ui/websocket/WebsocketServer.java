2f449fda021aa46f3e3607443c0f2bdb66860d0c
aptmac <aptmac@ibm.com>
2025-12-03 13:15:11 -0500

    tried to add the existing handlers

--- application/org.openjdk.jmc.ui.websocket/src/main/java/org/openjdk/jmc/ui/websocket/WebsocketServer.java
+++ application/org.openjdk.jmc.ui.websocket/src/main/java/org/openjdk/jmc/ui/websocket/WebsocketServer.java
@@ -33,17 +33,40 @@
  */
 package org.openjdk.jmc.ui.websocket;
 
+import java.io.IOException;
+import java.time.Duration;
+import java.util.HashMap;
+import java.util.List;
+import java.util.concurrent.CopyOnWriteArrayList;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
+import java.util.concurrent.TimeoutException;
+import java.util.function.Function;
 import java.util.logging.Level;
+import java.util.stream.Collectors;
+
 import org.eclipse.jetty.ee9.servlet.ServletContextHandler;
+import org.eclipse.jetty.ee9.websocket.api.Session;
+import org.eclipse.jetty.ee9.websocket.api.WebSocketAdapter;
 import org.eclipse.jetty.ee9.websocket.server.config.JettyWebSocketServletContainerInitializer;
 import org.eclipse.jetty.server.Server;
 import org.eclipse.jetty.server.ServerConnector;
+import org.openjdk.jmc.common.item.IItemCollection;
+import org.openjdk.jmc.flightrecorder.serializers.dot.DotSerializer;
+import org.openjdk.jmc.flightrecorder.serializers.json.FlameGraphJsonSerializer;
+import org.openjdk.jmc.flightrecorder.serializers.json.IItemCollectionJsonSerializer;
+import org.openjdk.jmc.flightrecorder.stacktrace.FrameSeparator;
+import org.openjdk.jmc.flightrecorder.stacktrace.FrameSeparator.FrameCategorization;
+import org.openjdk.jmc.flightrecorder.stacktrace.graph.StacktraceGraphModel;
+import org.openjdk.jmc.flightrecorder.stacktrace.tree.StacktraceTreeModel;
 
 public class WebsocketServer {
 	private Server server;
+	private List<WebsocketConnectionHandler> handlers = new CopyOnWriteArrayList<>();
+	private List<WebsocketConnectionHandler> treeHandlers = new CopyOnWriteArrayList<>();
+	private List<WebsocketConnectionHandler> graphHandlers = new CopyOnWriteArrayList<>();
 	private final ExecutorService executorService = Executors.newSingleThreadExecutor();
+	private IItemCollection currentSelection = null;
 
 	public WebsocketServer(int port) {
 		executorService.execute(() -> startServer(port));
@@ -60,10 +83,31 @@ public class WebsocketServer {
 		context.setContextPath("/");
 		server.setHandler(context);
 
-		// JettyWebSocketServletContainerInitializer.configure(context, (servletContext, container) -> {
-		// 	container.setMaxBinaryMessageSize(Long.MAX_VALUE);
-		// 	container.addMapping("/cryostat/*", new WebsocketEndpointCreator());
-		// });
+		 JettyWebSocketServletContainerInitializer.configure(context, (servletContext, container) -> {
+		 	container.setMaxBinaryMessageSize(Long.MAX_VALUE);
+		 	container.setIdleTimeout(Duration.ofHours(5));
+		 	container.addMapping("/events/*", (req, resp) -> {
+				// try to send the current selection when the client connects
+				// for simplicity, we serialise for every new connection
+				String eventsJson = WebsocketServer.toEventsJsonString(currentSelection);
+				WebsocketConnectionHandler handler = new WebsocketConnectionHandler(eventsJson);
+				handlers.add(handler);
+				return handler;
+		 	});
+			container.addMapping("/tree/*", (req, resp) -> {
+				String treeJson = WebsocketServer.toTreeModelJsonString(currentSelection);
+				WebsocketConnectionHandler handler = new WebsocketConnectionHandler(treeJson);
+				treeHandlers.add(handler);
+				return handler;
+			});
+			container.addMapping("/graph/*", (req, resp) -> {
+				String dot = WebsocketServer.toGraphModelDotString(currentSelection);
+				WebsocketConnectionHandler handler = new WebsocketConnectionHandler(dot);
+				graphHandlers.add(handler);
+				return handler;
+			});
+//		 	container.addMapping("/cryostat/*", new WebsocketEndpointCreator());
+		 });
 
 		try {
 			server.start();
@@ -73,7 +117,120 @@ public class WebsocketServer {
 		}
 	}
 
+	public void notifyAll(IItemCollection events) {
+		currentSelection = events;
+		notifyAllEventHandlers(events);
+		notifyAllGraphHandlers(events);
+		notifyAllTreeHandlers(events);
+	}
+
+	private void notifyAllEventHandlers(IItemCollection events) {
+		handlers = notifyAllHandlers(events, handlers, WebsocketServer::toEventsJsonString);
+	}
+
+	private void notifyAllGraphHandlers(IItemCollection events) {
+		graphHandlers = notifyAllHandlers(events, graphHandlers, WebsocketServer::toGraphModelDotString);
+	}
+
+	private void notifyAllTreeHandlers(IItemCollection events) {
+		treeHandlers = notifyAllHandlers(events, treeHandlers, WebsocketServer::toTreeModelJsonString);
+	}
+
+	private static String toEventsJsonString(IItemCollection items) {
+		if (items == null) {
+			return null;
+		}
+		return IItemCollectionJsonSerializer.toJsonString(items);
+	}
+
+	private static String toGraphModelDotString(IItemCollection items) {
+		if (items == null) {
+			return null;
+		}
+		FrameSeparator frameSeparator = new FrameSeparator(FrameCategorization.METHOD, false);
+		StacktraceGraphModel model = new StacktraceGraphModel(frameSeparator, items, null);
+		return DotSerializer.toDot(model, 10_000, new HashMap<>());
+	}
+
+	private static String toTreeModelJsonString(IItemCollection items) {
+		if (items == null) {
+			return null;
+		}
+		StacktraceTreeModel model = new StacktraceTreeModel(items);
+		return FlameGraphJsonSerializer.toJson(model);
+	}
+
+	private List<WebsocketConnectionHandler> notifyAllHandlers(
+			IItemCollection events, List<WebsocketConnectionHandler> handlers,
+			Function<IItemCollection, String> jsonSerializer) {
+			handlers = handlers.stream().filter(h -> h.isConnected()).collect(Collectors.toList());
+			if (handlers.size() == 0 || events == null) {
+				// do nothing if no handlers are registered
+				return handlers;
+			}
+			String json = jsonSerializer.apply(events);
+			handlers.forEach(handler -> handler.sendMessage(json));
+			return handlers;
+		}
+	
 	public void shutdown() throws Exception {
 		server.stop();
 	}
+	
+	private static class WebsocketConnectionHandler extends WebSocketAdapter {
+		private String firstMessage;
+
+		WebsocketConnectionHandler(String firstMessage) {
+			this.firstMessage = firstMessage;
+		}
+
+		public void sendMessage(String message) {
+			if (getSession() != null && isConnected()) {
+				WebsocketPlugin.getLogger().log(Level.INFO,
+						"Sending message to " + getSession().getRemoteAddress().toString());
+				try {
+					getSession().getRemote().sendString(message);
+				} catch (IOException e) {
+					WebsocketPlugin.getLogger().log(Level.SEVERE, "Failed to send websocket message", e);
+				}
+			}
+		}
+
+		@Override
+		public void onWebSocketConnect(Session sess) {
+			super.onWebSocketConnect(sess);
+			WebsocketPlugin.getLogger().log(Level.INFO,
+					"Socket connected to " + sess.getRemoteAddress().toString());
+			try {
+				if (firstMessage != null) {
+					getSession().getRemote().sendString(firstMessage);
+					firstMessage = null;
+				}
+			} catch (IOException e) {
+				WebsocketPlugin.getLogger().log(Level.SEVERE, "Failed to show outline view", e);
+			}
+		}
+
+		@Override
+		public void onWebSocketText(String message) {
+			super.onWebSocketText(message);
+		}
+
+		@Override
+		public void onWebSocketClose(int statusCode, String reason) {
+			super.onWebSocketClose(statusCode, reason);
+			WebsocketPlugin.getLogger().log(Level.INFO, "Socket closed: [" + statusCode + "] " + reason);
+		}
+
+		@Override
+		public void onWebSocketError(Throwable cause) {
+			super.onWebSocketError(cause);
+			if (cause.getCause() instanceof TimeoutException) {
+				WebsocketPlugin.getLogger().log(Level.INFO, "Websocket timed out");
+			} else {
+				WebsocketPlugin.getLogger().log(Level.SEVERE, "Websocket error", cause);
+			}
+		}
+	}
 }
+
